6839099d2410d41353f35a1c43b9a09b5f7e037b
SoulDancer27 <igorpanin10@gmail.com>
2024-05-05 14:44:23 +0300

    df

--- currency/pytorch/CurrencyDataset.py
+++ currency/pytorch/CurrencyDataset.py
@@ -3,16 +3,22 @@ import pandas as pd
 from torch.utils.data import Dataset
 
 class CurrencyDataset(Dataset):
-    def __init__(self, csv_file):
+    def __init__(self, data, seq_length = 1, columns = None):
         """
         Arguments:
             csv_file (string): Path to the csv file with data
+            columns: list of columns from a dataframe
         """
-        data = pd.read_csv(csv_file)
-        self.data = data[['open', 'high', 'close', 'vol']]
+        data = data
+        self.data = data[columns] if columns else data
+        self.seq_length = seq_length
+        self.total_length = len(data) - seq_length
 
     def __len__(self):
         return len(self.data)
 
-    def __getitem__(self, idx):
-        return torch.tensor(self.data.iloc[idx].values)
+    def __getitem__(self, index):
+        if index < self.total_length:
+            sequence = self.data.iloc[index: index + self.seq_length].values
+            target = self.data.iloc[index + self.seq_length].values
+            return {'data': torch.tensor(sequence), 'target': torch.tensor(target, dtype=torch.float32)}
